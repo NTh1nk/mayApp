ee4822a818ce501c6b22eb4e402e17189311fbd1
Johannes <Johannesslb@gmail.com>
2025-05-11 19:43:16 +0200

    Update placeMarker.js

--- GMC/src/placeMarker.js
+++ GMC/src/placeMarker.js
@@ -8,6 +8,9 @@ export function markers(){
     // import geolocated position
     let address = processInput();
     let coordinates = geocodeAddress();
+    let numberOfCoodinates = 1;
+    const ngData = []
+
     //30 random example markers
     const N = 30;
     const gData = [...Array(N).keys()].map(() => ({
@@ -16,6 +19,19 @@ export function markers(){
         size: 7 + Math.random() * 30,
         color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
         }));
-    return gData;
+    return gData; //change this up when finished
 
+}
+
+
+
+function coToMarker(coordinates){
+
+    const marker =  {
+        lat: coordinates.lat,
+        lng: coordinates.lng,
+        size: 15,// make this scale with the amount of people
+        color: ['red', 'white', 'blue', 'green'][Math.round(Math.random() * 3)]
+    }
+    return marker;
 }

ad62ec3d2519b2c515afdae1a386f65aa4beb3da
Johannes <Johannesslb@gmail.com>
2025-05-11 22:02:52 +0200

    Update geocodeAddress.js

--- GMC/src/geocodeAddress.js
+++ GMC/src/geocodeAddress.js
@@ -1,14 +1,15 @@
-export function geocodeAddress(address, callback) {
+export function geocodeAddress(address) {
     const geocoder = new google.maps.Geocoder();
 
-    geocoder.geocode({ address: address }, function(results, status) {
-        if (status === 'OK' && results[0]) {
-            const lat = results[0].geometry.location.lat();
-            const lng = results[0].geometry.location.lng();
-            callback({ lat, lng }); // return coordinates through the callback
-        } else {
-            console.error('Geocode failed: ' + status);
-            callback(null); // return null on failure
-        }
+    return new Promise((resolve, reject) => {
+        geocoder.geocode({ address: address }, function(results, status) {
+            if (status === 'OK' && results[0]) {
+                const lat = results[0].geometry.location.lat();
+                const lng = results[0].geometry.location.lng();
+                resolve({ lat, lng });
+            } else {
+                reject(new Error('Geocode failed: ' + status));
+            }
+        });
     });
 }

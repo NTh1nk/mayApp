c373ce14afcaf950ac41cc6b8b66192efc6e9b67
Johannes <Johannesslb@gmail.com>
2025-05-11 18:44:46 +0200

    Create algo.js

---
+++ GMC/src/globe/algo.js
@@ -0,0 +1,15 @@
+//Import Input
+import { processInput } from './processInput.js';
+
+
+const people = [
+    //example
+    {timeZone: 'Europe/Copenhagen', weight: 2, workStart: 8 * 60, workEnd: 16 * 60} 
+]
+
+
+function getLocalTime(UTCTime, timeZone)
+{
+
+    
+}

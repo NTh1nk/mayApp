828ef643ce34f732bec6a8184e91757288c95e86
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2025-05-16 11:58:40 +0200

    TZ lookup test

--- DevTesting/TimeZone/TimeZone.js
+++ DevTesting/TimeZone/TimeZone.js
@@ -1,1 +1,16 @@
 //Should take the coords, and get the timezone
+
+//Using node tz lookup
+const tz_lookup = require("tz-lookup");
+const tz = require("tz-lookup");
+
+
+//import coords from geocache in prod.
+//Example coords
+
+let coords = { lat: 55.722842, lng: 12.579473, amount:35 }
+
+
+//log timezone
+
+console.log(tz_lookup(coords.lat, coords.lng));

09b7699e8f9cf15d27e05700bc95c0f889a3d9a2
Johannes <Johannesslb@gmail.com>
2025-05-19 13:45:58 +0200

    Started on geocode,
    Broken rn

--- GMC/src/geocodeAddress.js
+++ GMC/src/geocodeAddress.js
@@ -1,35 +1,27 @@
-/*
-//Needs a rework, so that it can run on a webserver
 
-
-require('env/dotenv').config();  // Load environment variables from .env file
-
-
-
-//require doenst work in npm    
-const fetch = require("node-fetch");
-// Access the OpenCage API key from the environment variable
-const apiKey = process.env.OPENCAGE_API_KEY;
-
-export function geocodeAddress(address) {
-    // OpenCage API endpoint
-    const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${apiKey}`;
-
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                if (data.results && data.results[0]) {
-                    const lat = data.results[0].geometry.lat;
-                    const lng = data.results[0].geometry.lng;
+export function geocodeAddress(address){
+    return new Promise((resolve, reject) =>{
+        fetch("/api/geocode?addres=${encodeURIComponent(address)}")
+            .then(res => res.json())
+            .then(data =>{
+                if(data.lat && data.lng){
+                    //log error
+                    console.log("Coordinates", data.lat, data.lng)
                     resolve({ lat, lng });
-                } else {
+
+                } else{
+                    //error
                     reject(new Error('Geocode failed: No results found.'));
-                }
+
             })
             .catch(error => {
                 reject(new Error('Geocode failed: ' + error));
+
             });
-    });
+
+
+
+    })
+    
+
 }
-*/
---
+++ GMC/src/geocodeAddressOld.js
@@ -0,0 +1,34 @@
+
+//Needs a rework, so that it can run on a webserver
+
+
+require('env/dotenv').config();  // Load environment variables from .env file
+
+
+
+//require doenst work in npm    
+const fetch = require("node-fetch");
+// Access the OpenCage API key from the environment variable
+const apiKey = process.env.OPENCAGE_API_KEY;
+
+export function geocodeAddress(address) {
+    // OpenCage API endpoint
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${apiKey}`;
+
+    return new Promise((resolve, reject) => {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                if (data.results && data.results[0]) {
+                    const lat = data.results[0].geometry.lat;
+                    const lng = data.results[0].geometry.lng;
+                    resolve({ lat, lng });
+                } else {
+                    reject(new Error('Geocode failed: No results found.'));
+                }
+            })
+            .catch(error => {
+                reject(new Error('Geocode failed: ' + error));
+            });
+    });
+}

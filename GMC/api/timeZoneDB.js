6f8c49316ed662bc7ce13caaf8bdcd6fd5720f8e
Johannes <Johannesslb@gmail.com>
2025-05-22 08:46:35 +0200

    Update timeZoneDB.js

--- GMC/api/timeZoneDB.js
+++ GMC/api/timeZoneDB.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     // Accept lat and lng as query parameters
     const { lat, lng } = req.query;
-
+    console.log("Received lat:", lat, "lng:", lng);
     // Query validation
     if (!lat || !lng) {
         res.status(400).json({ error: "Missing lat or lng query parameter" });

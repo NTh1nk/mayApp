f617e63d9e02fb1fc079a9d1571e43ee68f4266f
Johannes <Johannesslb@gmail.com>
2025-05-25 14:06:10 +0200

    Update main.js

--- GMC/public/js/main.js
+++ GMC/public/js/main.js
@@ -1,12 +1,12 @@
 import { processInput } from './processInput.js';
 import { initGlobe } from './globe.js';
 import { markers } from './placeMarker.js';
-import data from './markers.json';
+//import data from './markers.json';
 import { cOMT } from './COMT.js';
 import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
 import { getTimeZone } from './tzDB.js';
 
-console.log(data);
+//console.log(data);
 //let updatedMarkers = [...data];
 let updatedMarkers = [];
 let addressData = [];

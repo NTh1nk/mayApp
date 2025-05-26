7fc352e3d19a4f4fc158e774e33e1755151ced0a
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2025-05-26 22:31:25 +0200

    made a dupe for testing

---
+++ GMC/public/src/js/homepage.js
@@ -0,0 +1,71 @@
+import { processInput } from './processInput.js';
+import { initGlobe } from './globe.js';
+import { markers } from './placeMarker.js';
+//import data from './markers.json';
+import { cOMT } from './COMT.js';
+import * as THREE from 'https://unpkg.com/three@0.152.2/build/three.module.js';
+import { getTimeZone } from './tzDB.js';
+
+//console.log(data);
+//let updatedMarkers = [...data];
+let updatedMarkers = [];
+let addressData = [];
+let lastInputData = null;
+
+async function handleInsert(event) {
+  event.preventDefault();
+
+  const inputData = await processInput(event);
+  if (inputData?.error) {
+    console.warn("Input error:", inputData?.error);
+    return;
+  }
+  const timezone = await getTimeZone(inputData.coords.lat, inputData.coords.lng);
+  lastInputData = inputData;
+
+  // Merge coords into the object for COMT
+  console.log("Input data:", inputData);
+  addressData.push({
+    ...inputData.coords,
+    workStart: 480, // Could also get input here
+    workEnd: 1200,  // Could also get input here
+    timezone,       // Add timezone to address data
+    // add other fields as needed
+  });
+
+  updatedMarkers = updatedMarkers.concat(await markers(inputData));
+  console.log("Updated markers:", updatedMarkers)
+  initGlobe({ coordinateArray: updatedMarkers });
+
+  // Clear inputs and refocus
+  const locationInput = document.getElementById("location");
+  const amountInput = document.getElementById("amount");
+
+  locationInput.value = '';
+  amountInput.value = '';
+  locationInput.focus();
+}
+
+async function handleCalc(event) {
+  event.preventDefault();
+  console.log("Calculation button clicked");
+  if (addressData.length === 0) {
+    alert("Please insert at least one location before calculating.");
+    return;
+  }
+
+  const OMTBox = document.getElementById("omtResult");
+  const OMT = await cOMT(addressData);
+  console.log("Optimal Meeting Time (UTC):", OMT);
+  OMTBox.value = OMT/60 + ":00 UTC";
+  return OMT;
+}
+// Attach event listener after DOM is loaded
+window.addEventListener('DOMContentLoaded', () => {
+  window.processInput = processInput;
+  document.getElementById('insertBtn').addEventListener('click', handleInsert);
+  document.getElementById('calcBtn').addEventListener("click", handleCalc);
+
+  // Initialize globe with no markers
+  initGlobe({ coordinateArray: [] });
+});

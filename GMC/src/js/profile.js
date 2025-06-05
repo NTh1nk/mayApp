8164177ea9e805cfdedee261a067c761794af931
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2025-06-05 19:32:18 +0200

    started on the profile stuff

--- GMC/src/js/profile.js
+++ GMC/src/js/profile.js
@@ -1,18 +1,29 @@
 export function showProfile( ) {
     const userName = localStorage.getItem("userName");
     const welcomeEl =  document.getElementById("welcomeMessage");
-
+    const profileEmailEl = document.getElementById("profileEmail");
+    const profilePictureEl = document.getElementById("profilePicture");
     console.log("User profile initialized");
+
+    if (!userName) {
+        console.log("No user is logged in.");
+        return;
+    }
     if (welcomeEl && userName) {
         welcomeEl.textContent = `Welcome, ${userName}!`;
         console.log("User profile displayed:", userName);
 
     }
-
-
+    if (profileEmailEl && profilePictureEl) {
+        //Takes the stuff from local storage and displays it
+        const userEmail = localStorage.getItem("userEmail");
+        const userPicture = localStorage.getItem("userPicture")
+        profileEmailEl.textContent = `Email: ${userEmail}`;
+        profilePictureEl.innerHTML = `Profile Picture: <img id="picture" src="${userPicture}" alt="Profile Picture">`;
+    }
 }
 
-
+    
 export function logout() {
     localStorage.removeItem("userName");
     const welcomeEl = document.getElementById("welcomeMessage");
@@ -23,4 +34,5 @@ export function logout() {
     console.log("User logged out");
     window.location.reload();
 }
+
 

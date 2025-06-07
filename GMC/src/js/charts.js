0fe9c5adbff83a8ae75481da6c9f0db13d75d703
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2025-06-07 17:43:12 +0200

    Update charts.js

--- GMC/src/js/charts.js
+++ GMC/src/js/charts.js
@@ -26,11 +26,20 @@ const ctx = canvas.getContext("2d");
   window.addEventListener("resize", resizeCanvas);
   resizeCanvas();
 
-const xValues = ['00:00', '01:00'];
-const yValues = [
-  localStorage.getItem("flaw0") || 0,
-  localStorage.getItem("flaw1") || 0,
+const xValues = ['00:00', '01:00', '02:00', '03:00', '04:00', '05:00', '06:00', '07:00', '08:00', '09:00', 
+'10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00', '18:00', '19:00', '20:00', '21:00', '22:00', '23:00'];
+
+let yValues = [
+
 ]
+
+//loop through the xValues and get the localStorage values for each hour
+for (let i = 0; i < xValues.length; i++) {
+    //const hour = i.toString().padStart(2, '0') + ":00";
+    //const storedValue = localStorage.getItem(hour) || 0;
+    yValues.push(localStorage.getItem(i*60 + "time") || 0);
+}
+
 
 
 new Chart("flawChart", {

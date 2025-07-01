481531761b9e7b65d3136238a44df7f1af4c311f
NTh1nk <83032430+NTh1nk@users.noreply.github.com>
2025-07-01 15:07:51 +0200

    made the navbar disable on mobile

--- GMC/src/js/bottombar.js
+++ GMC/src/js/bottombar.js
@@ -1,5 +1,8 @@
+import { isDesktop } from './mobile';
 import { loadBottomBar } from '/src/js/navbarimport.js';
 window.addEventListener('DOMContentLoaded', () => {
+  if(isDesktop(window)){
   loadBottomBar();
+  }
 });
 
